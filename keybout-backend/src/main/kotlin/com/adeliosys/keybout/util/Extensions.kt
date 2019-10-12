77c643079b8aeee5d5387c66757be703f45a972b
Florian Beaufumé <fbeaufume@gmail.com>
2019-10-12 15:51:19 +0200

    Added logs about the actual SockJS transport

--- keybout-backend/src/main/kotlin/com/adeliosys/keybout/util/Extensions.kt
+++ keybout-backend/src/main/kotlin/com/adeliosys/keybout/util/Extensions.kt
@@ -3,6 +3,9 @@ package com.adeliosys.keybout.util
 import com.adeliosys.keybout.model.ClientState
 import org.slf4j.Logger
 import org.springframework.web.socket.WebSocketSession
+import org.springframework.web.socket.sockjs.transport.session.PollingSockJsSession
+import org.springframework.web.socket.sockjs.transport.session.StreamingSockJsSession
+import org.springframework.web.socket.sockjs.transport.session.WebSocketServerSockJsSession
 
 // WebSocketSession extensions
 
@@ -25,12 +28,20 @@ val WebSocketSession.state: ClientState
 fun WebSocketSession.setState(logger: Logger, state: ClientState, gameId: Long, userCount: Int = 0) {
     attributes[STATE] = state
     attributes[GAME_ID] = gameId
-    logger.debug("Changed state to '{}' and game #{} for {}{}",
+    logger.debug("Changed state to '{}' and game #{} for {}{}{}",
             state,
             gameId,
             description,
+            if (state == ClientState.OPENED) " with '${getTransportName()}' transport" else "",
             if (userCount > 0) ", user count is $userCount" else "")
 }
 
 val WebSocketSession.gameId: Long
     get() = attributes[GAME_ID] as Long
+
+fun WebSocketSession.getTransportName(): String = when (this) {
+    is WebSocketServerSockJsSession -> "WebSocket"
+    is StreamingSockJsSession -> "Streaming"
+    is PollingSockJsSession -> "Polling"
+    else -> "unknown"
+}
