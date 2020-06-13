b61ae559c5fd271ec3b3d4830813443b96ecac91
Florian Beaufumé <fbeaufume@gmail.com>
2020-06-13 16:28:27 +0200

    Simplified the MongoDB configuration

--- keybout-backend/src/main/kotlin/com/adeliosys/keybout/config/MongoConfig.kt
+++ keybout-backend/src/main/kotlin/com/adeliosys/keybout/config/MongoConfig.kt
@@ -1,17 +1,13 @@
 package com.adeliosys.keybout.config
 
 import com.mongodb.MongoClient
-import com.mongodb.MongoClientOptions
-import org.springframework.beans.factory.ObjectProvider
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.boot.autoconfigure.mongo.MongoClientFactory
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.autoconfigure.mongo.MongoProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.env.Environment
 
 /**
  * Custom MongoDB configuration class inspired by [org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration].
@@ -23,12 +19,4 @@ import org.springframework.core.env.Environment
 @EnableConfigurationProperties(MongoProperties::class)
 @ConditionalOnMissingBean(type = ["org.springframework.data.mongodb.MongoDbFactory"])
 @ConditionalOnProperty("spring.data.mongodb.uri")
-// TODO FBE simplify this
-class MongoConfig {
-
-    @Bean
-    @ConditionalOnMissingBean(type = ["com.mongodb.MongoClient", "com.mongodb.client.MongoClient"])
-    fun mongo(properties: MongoProperties?, options: ObjectProvider<MongoClientOptions?>, environment: Environment?): MongoClient? {
-        return MongoClientFactory(properties, environment).createMongoClient(options.ifAvailable)
-    }
-}
+class MongoConfig : MongoAutoConfiguration()
