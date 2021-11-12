42b0e99445f59a49224aa17a7cbe63a168e7c38b
Florian Beaufumé <fbeaufume@gmail.com>
2021-11-12 18:52:50 +0100

    Added an integration test

---
+++ keybout-backend/src/test/kotlin/com/adeliosys/keybout/KeyboutApplicationTest.kt
@@ -0,0 +1,12 @@
+package com.adeliosys.keybout
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KeyboutApplicationTest {
+
+    @Test
+    fun contextLoads() {
+    }
+}
